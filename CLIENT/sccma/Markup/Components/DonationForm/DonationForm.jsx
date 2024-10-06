4adb3337ddc9d088239b25829561242f7a4e4c87
Yonas Negese M <yonasnegese250@gmail.com>
2024-10-06 20:45:48 +0300

    Finish the donation Form

--- CLIENT/sccma/Markup/Components/DonationForm/DonationForm.jsx
+++ CLIENT/sccma/Markup/Components/DonationForm/DonationForm.jsx
@@ -6,29 +6,74 @@ function DonationForm() {
                 <h2>Donation Form</h2>
                 <div className={styles.form_container}>
                     <div className={styles.userInfo_wrapper}>
-                        <label htmlFor="fullName">Donor Name:</label>
-                        <input type="text" name='fullName'/>
-                        <label htmlFor="email">Email: </label>
-                        <input type="email" name="email" id="" />
+                      <div>
+                          <label htmlFor="fullName">Donor Name</label>
+                          <input type="text" name='fullName' placeholder="Enter your Full Name" required/>
+                      </div>
+                      <div>
+                        <label htmlFor="email">Email </label>
+                        <input type="email" name="email" id="" placeholder="Enter your Email" required/>
+                      </div>
                     </div>
                     <div className={styles.address_wrapper}>
-                        <label htmlFor="address">Address: </label>
+                        <label htmlFor="address">Address </label>
                         <input type="text" name='address' placeholder='Enter your address'/>
                     </div>
                     <div className={styles.amount_wrapper}>
-                            <input type="checkbox" name="" id="" />
-                            <input type="checkbox" name="" id="" />
-                            <input type="checkbox" name="" id="" />
-                            <input type="checkbox" name="" id="" />
-                            <input type="checkbox" name="" id="" />
-                            <input type="checkbox" name="" id="" />
+                            <p>Donation amount</p>
+                            <div>
+                              <div className={styles.top_radios}>
+                                <div>
+                                  <input type="radio" name="ten" id="" />
+                                  <label htmlFor="">$10</label>
+                                </div>
+                                <div>
+                                  <input type="radio" name="twenty" id="" />
+                                  <label htmlFor="twenty">$20</label>
+                                </div>
+                                <div>
+                                  <input type="radio" name="fifty" id="" />
+                                  <label htmlFor="fifty">$50</label>
+                                </div>
+                              </div>
+                              <div className={styles.bottom_radios}>
+                                <div>
+                                  <input type="radio" name="hundred" id="" />
+                                  <label htmlFor="hundred">$100</label>
+                                </div>
+                                <div>
+                                  <input type="radio" name="fiveHundred" id="" />
+                                  <label htmlFor="fiveHundred">$500</label>
+                                </div>
+                                <div>
+                                  <input type="radio" name="custom" id="" />
+                                  <label htmlFor="custom">Custom</label>
+                                </div>
+                              </div>
+                            </div>
                     </div>
                     <div className={styles.payment_wrapper}>
-                            <input type="checkbox" name="" id="" />
-                            <input type="checkbox" name="" id="" />
-                            <input type="checkbox" name="" id="" />
+                            <p>Payment method</p>
+                            <div>
+                              <div className={styles.top_radios}>
+                                <div>
+                                    <input type="radio" name="paypal" id="" />
+                                    <label htmlFor="paypal">Paypal</label>
+                                </div>
+                                <div>
+                                    <input type="radio" name="chapa" id="" />
+                                    <label htmlFor="chapa">Chapa</label>
+                                </div>
+                              </div>
+                            </div>
+                            <div className={styles.bottom_radios}>
+                              <div>
+                                  <input type="radio" name="masterCard" id="" />
+                                  <label htmlFor="masterCard">MasterCard</label>
+                              </div>
+                            </div>
                     </div>  
-                    <input type="submit" value="Donate Now" />
+                   <a href="#" className={styles.donate_btn}>Submit</a>
                 </div>
         </div>
     </>
