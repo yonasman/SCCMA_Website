f7021a39adf971f3458b9dab78ba470cf303c44f
Yonas Negese M <yonasnegese250@gmail.com>
2024-10-12 21:40:53 +0300

    Modify rotating cards component

--- CLIENT/sccma/Markup/Components/LearnMore/LearnMore.jsx
+++ CLIENT/sccma/Markup/Components/LearnMore/LearnMore.jsx
@@ -8,12 +8,12 @@ function LearnMore() {
                         {/* left video container */}
                         <div className={styles.left_video_wrapper}>
                                 <iframe 
-                                    width="100%" 
-                                    height="100%" 
-                                    src="https://www.youtube.com/embed/hw_V3WpHUwo" 
-                                    frameBorder="0" 
-                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                                    allowfullscreen>
+                                        width="100%" 
+                                        height="100%" 
+                                        src="https://www.youtube.com/embed/hw_V3WpHUwo" 
+                                        frameBorder="0" 
+                                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                                        allowfullscreen>
                                 </iframe>
                                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias adipisci nihil est deleniti.</p>
                         </div>
