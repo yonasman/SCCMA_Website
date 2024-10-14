499ddb467b91dbc7f2395b037c97df5b1d8fe140
Yonas Negese M <yonasnegese250@gmail.com>
2024-10-14 15:52:58 +0300

    Modify contact form component

--- CLIENT/sccma/Markup/Components/Justification/Justification.jsx
+++ CLIENT/sccma/Markup/Components/Justification/Justification.jsx
@@ -5,7 +5,7 @@ function Justification() {
     <>
         <div className={styles.justification_container}>
             <div className={styles.justification_text_container}>
-                <h2>Background and Justification</h2>
+                <h1>Background and Justification</h1>
                 <p>Despite Ethiopia's economic growth, over 23% of its population still lives below the poverty line (UNDP Ethiopia, 2018). Children are hit hardest, with 13 million living in poverty, and 2 million in extreme poverty (UNICEF Ethiopia, 2016). They face greater risks than adults, especially in households headed by informal sector workers, where 13.1% of children live in extreme poverty. Urban poor children are increasingly vulnerable to street life due to poverty, lack of education, and family instability.
                 </p>
                 <h3>Your donation can help protect these children and offer them a brighter future.</h3>
